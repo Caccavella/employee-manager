4ccfa1a07cc50f52d958896b861124e174ed4208
Anthony <anthony.caccavella@gmail.com>
2017-08-14 15:04:21 -0600

    Got through stage 8 and had a panic attack because I hadn't pushed for the whole project.

--- src/Stage 2/components/EmployeeEditor.js
+++ src/Stage 2/components/EmployeeEditor.js
@@ -8,8 +8,17 @@ class EmployeeEditor extends Component {
       originalEmployee: null,
       notModified: true
     };
+    this.save = this.save.bind(this);
+    this.cancel = this.cancel.bind(this);
   }
 
+  componentWillReceiveProps(props) {
+    this.setState ({ 
+      employee: Object.assign({}, props.selected),
+      originalEmployee: props.selected,
+      notModified: true
+    })
+  }
   // componentWillReceiveProps
 
   handleChange(prop, val) {
